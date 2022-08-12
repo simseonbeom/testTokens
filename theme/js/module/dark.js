fa87d300e9ced16e8313ae266211e42564f3b75e
kindtiger <seonbeom2@gmail.com>
2022-08-12 19:59:53 +0900

    npm 명령어 및 githubaction 추가

---
+++ theme/js/module/dark.js
@@ -0,0 +1,19 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 12 Aug 2022 10:41:41 GMT
+ */
+
+module.exports = {
+  "primary": "#ffffff",
+  "secondary": "#000000",
+  "tertiary": "#2b2b2b",
+  "contents": {
+    "contentPrimary": "#ffffff",
+    "contentSecondary": "#a6a6a6",
+    "contentTertiary": "#898989"
+  },
+  "border": {
+    "borderOpaque": "#c4c4c4",
+    "borderTransparent": "#8989894d"
+  }
+}
